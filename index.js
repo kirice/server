26773fdd135a482e4e9532edbf416280573b8407
kirice <kirice8@gmail.com>
2025-06-10 00:33:29 +0300

    Add dotenv dependency

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS
 app.use(cors({
-  origin: ['http://localhost:5501'],
+  origin: ['https://gruesomely-prophetic-damselfly.cloudpub.ru/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
