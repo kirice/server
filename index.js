8ff2317cef7b1fb66b4be1f9a39a4e89ce687509
kirice <71143779+kirice@users.noreply.github.com>
2025-06-11 02:02:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS
 app.use(cors({
-  origin: ['https://frontend-production-0e8f.up.railway.app/'],
+  origin: ['https://frontend-production-0e8f.up.railway.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
